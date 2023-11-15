30cb7f5760a870fd9e27a29ad277508b30a573e2
Robert Dinterman <robert.dinterman@gmail.com>
2023-11-15 01:24:27 +0000

    remove shell credentials

--- github.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-git config --global user.email "robert.dinterman@gmail.com"
-git config --global user.name "Robert Dinterman"
-
-git config --global credential.helper 'cache --timeout 3600'
-
-git config --global --list
