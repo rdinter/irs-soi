2ad1361a76b72a373e1d1bc0aef01de0f10cc40e
Robert Dinterman <robert.dinterman@gmail.com>
2025-01-29 13:42:33 +0000

    duck version of ZIP/ZCTA

--- 1-tidy/1-zipcode-zctas.R
+++ 1-tidy/1-zipcode-zctas.R
@@ -1,10 +1,38 @@
-# Find the ZIP code centroids based on ZCTA then elsewhere
+# Tidy version of ZIP code and ZCTA relationship
 
 # ---- start --------------------------------------------------------------
 
 library(tidyverse)
-library(tigris)
-library(httr2)
+library(duckdb)
+library(RSQLite)
+
+# Create a directory for the data, ignore for GitHub
+duck_dir <- "0-data/duckdb"
+
+# Connect to or start the DB
+duck_con <- duckdb::dbConnect(
+  duckdb::duckdb(dbdir = str_glue("{duck_dir}/irs_soi.duckdb"))
+)
+
+duckdb::dbListTables(duck_con)
+
+irs_zip <- tbl(duck_con, "irs_soi_zip")
+full_monty <- irs_zip |>
+  select(year, state, zip_code, agi_desc,
+         return, exmpt, dependents,
+         agi, wages, interest, earned_income, tax_liability) |> 
+  left_join(tbl(duck_con, "zip_codes_geo")) |>
+  filter(agi_desc == "All", !is.na(lat)) |> 
+  collect()
+
+
+
+# Expand gitignore
+gitignore <- paste0(str_remove(local_dir, "/raw"), "/.gitignore")
+if (!file.exists(gitignore)) write.table("raw",
+                                         file = gitignore,
+                                         quote = FALSE, sep = "\n",
+                                         col.names = FALSE, row.names = FALSE)
 
 # Create a directory for the data, ignore for GitHub
 local_dir <- "1-tidy/ZIP"
@@ -33,19 +61,34 @@ st_fips <- fips_codes |>
 st_cross <- st_fips$st_fips
 names(st_cross) <- st_fips$st_abbr
 
-zctas <- read_csv("0-data/ZCTA/nber_zcta_centroids.csv") |> 
+nber_zctas <- read_csv("0-data/ZCTA/nber_zcta_centroids.csv") |> 
   mutate(uzip = paste0(st_fips, zcta5))
-irs_z <- read_csv("0-data/IRS/zipcode/irs_zipcode_no_agi_xls.csv") |> 
-  mutate(st_fips = st_cross[st_abbr],
-         uzip = paste0(st_fips, zipcode))
-zipcode_dot_com <- read_csv("0-data/ZCTA/zip_code_database.csv") |> 
-  mutate(latitude = ifelse(near(latitude, 0), NA, latitude),
-         longitude = ifelse(near(longitude, 0), NA, longitude))
+# zipcodeR
+zipcode_R <- zipcodeR::zip_code_db |> 
+  select(zipcode,
+         latitude = lat,
+         longitude = lng)
+
+# irs_z <- read_csv("0-data/IRS/zipcode/irs_zipcode_no_agi_xls.csv") |> 
+#   mutate(st_fips = st_cross[st_abbr],
+#          uzip = paste0(st_fips, zipcode))
+# zipcode_dot_com <- read_csv("0-data/ZCTA/zip_code_database.csv") |> 
+#   mutate(latitude = ifelse(near(latitude, 0), NA, latitude),
+#          longitude = ifelse(near(longitude, 0), NA, longitude))
+
+duck_con <- dbConnect(duckdb(), "0-data/duckdb/irs_soi.duckdb")
+irs_z <- tbl(duck_con, "irs_soi_zip") |>
+  filter(agi_desc == "All", zip_code != 99999) |> 
+  select(year, state, zip_code) |> 
+  distinct() |> 
+  collect() |> 
+  mutate(zipcode = str_pad(zip_code, 5, "left", "0"))
+
 
 # ---- join ---------------------------------------------------------------
 
 # First fill in all the ZCTAs
-filled_zctas <- zctas |> 
+filled_zctas <- nber_zctas |> 
   complete(year = 1990:2022, zcta5) |> 
   group_by(zcta5) |> 
   fill(zcta5, lon, lat, .direction = "downup") |> 
@@ -55,18 +98,18 @@ irs_zcta <- left_join(irs_z, filled_zctas) |>
   rename(lon_zcta = lon, lat_zcta = lat)
 
 # Now add in the zipcode_dot_com for secondary replacement
-irs_dot_com <- zipcode_dot_com |> 
-  select(zipcode = zip, lat_zcdc = latitude, lon_zcdc = longitude) |> 
+irs_zipcoder <- zipcode_R |> 
+  select(zipcode, lat_zcdR = latitude, lon_zcdR = longitude) |> 
   left_join(x = irs_zcta, y = _) |> 
   # Cascade the ZIP codes by filling in the non-ZCTA centroids
-  mutate(lat = ifelse(is.na(lat_zcta), lat_zcdc, lat_zcta),
-         lon = ifelse(is.na(lon_zcta), lon_zcdc, lon_zcta))
+  mutate(lat = ifelse(is.na(lat_zcta), lat_zcdR, lat_zcta),
+         lon = ifelse(is.na(lon_zcta), lon_zcdR, lon_zcta))
 
 # ---- write --------------------------------------------------------------
 
-irs_dot_com |> 
+irs_zipcoder |> 
   select(year, state, st_abbr, st_fips, zipcode,
          exmpt, return, agi, wages, dividends, interest, taxes,
          lat, lon,
-         lon_zcta, lat_zcta, lon_zcdc, lat_zcdc) |> 
+         lon_zcta, lat_zcta, lon_zcdR, lat_zcdR) |> View()
   write_csv(paste0(local_dir, "/irs_zip_geocoded.csv"))
